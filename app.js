0c6a918e16e395685220d14a72f227fcf0710f12
Andrey Sorokin <pho.nzp@gmail.com>
2013-02-18 14:18:27 +0600

    Enable caching and compressing.

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ var path = require('path');
 var express = require('express'),
     app = express();
 var port = process.env.PORT || 3000;
+var oneYear = 86400000 * 365;
+
+app.use(express.compress());
 
 app.use(express.static(
-    path.join(__dirname, 'public')));
+    path.join(__dirname, 'public'), {maxAge: oneYear}));
 
 app.listen(port);
 console.log('Listening on port', port);
